283012c148d7d84da17a68784547c5cba555d43d
Brandon R. Stoner <github@monokro.me>
2010-08-02 13:35:48 -0600

    Added URL names for RSS and ATOM feeds.

--- django/contrib/news/urls.py
+++ django/contrib/news/urls.py
@@ -20,8 +20,8 @@ urlpatterns = patterns('',
     url(r'^article/(?P<identifier>[\d]+)/$','django.contrib.news.views.article', name='news_article'),
     url(r'^article/(?P<identifier>[^/]+)/$','django.contrib.news.views.article', {'slugified':True}, name='news_article'),
 
-    url(r'^rss/v1/(?P<url>.+)$', 'django.contrib.syndication.views.feed', {'feed_dict': rss_v1_feeds}),
-    url(r'^rss/v2/(?P<url>.+)$', 'django.contrib.syndication.views.feed', {'feed_dict': rss_v2_feeds}),
-    url(r'^atom/v1/(?P<url>.+)$', 'django.contrib.syndication.views.feed', {'feed_dict': atom_v1_feeds}),
+    url(r'^rss/v1/(?P<url>.+)$', 'django.contrib.syndication.views.feed', {'feed_dict': rss_v1_feeds}, name='news_feed_rss'),
+    url(r'^rss/v2/(?P<url>.+)$', 'django.contrib.syndication.views.feed', {'feed_dict': rss_v2_feeds}, name='news_feed_rss2'),
+    url(r'^atom/v1/(?P<url>.+)$', 'django.contrib.syndication.views.feed', {'feed_dict': atom_v1_feeds}, name='news_feed_atom'),
 )
 
