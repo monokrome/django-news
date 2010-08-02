02d6260861d3ddd01245c638ec22970c92647fef
Brandon R. Stoner <monokrome@monokro.me>
2010-08-01 22:41:38 -0600

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(name='django-news',
+      version='0.5',
+      description='A django application for keeping your fans up to date...',
+      author='Brandon R. Stoner',
+      author_email='monokrome@monokro.me',
+      url='http://monokro.me/',
+      packages=['django.contrib.news'],
+)
+
