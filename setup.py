0add9bb782b4d63591c4104d0b154ddcb1b2cb6a
Brandon R. Stoner <github@monokro.me>
2010-08-06 20:12:59 -0600

    Updated setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from distutils.core import setup
 
 setup(name='django-news',
-      version='0.5',
+      version='0.7',
       description='A django application for keeping your fans up to date...',
       author='Brandon R. Stoner',
       author_email='monokrome@monokro.me',
       url='http://monokro.me/',
+      download_url='http://github.com/monokrome/django-news/',
       packages=['django.contrib.news'],
+      data_files=[
+             (
+                 'django/contrib/news/templates/news/',
+                 [
+                     'django/contrib/news/templates/news/base.html',
+                     'django/contrib/news/templates/news/article_list.html',
+                     'django/contrib/news/templates/news/article_detail.html',
+                 ]
+             ),
+      ],
 )
 
