e7e3d7e5df4efc544800d8c6bc64cff546fdeaee
Brandon R. Stoner <github@monokro.me>
2010-08-06 19:25:59 -0600

    Fixed an issue in availability detection

--- django/contrib/news/availability.py
+++ django/contrib/news/availability.py
@@ -5,14 +5,14 @@ from django.conf import settings
 
 ###
 # Check if comments are available
-if hasattr(settings, 'INSTALLED_APPS') and 'django.contrib.comments' in settings['INSTALLED_APPS']:
+if hasattr(settings, 'INSTALLED_APPS') and 'django.contrib.comments' in settings.INSTALLED_APPS:
     comments = True
 else:
     comments = False
 
 ###
 # Check if markup is available
-if hasattr(settings, 'INSTALLED_APPS') and 'django.contrib.markup' in settings['INSTALLED_APPS']:
+if hasattr(settings, 'INSTALLED_APPS') and 'django.contrib.markup' in settings.INSTALLED_APPS:
     markup = True
     markup_filters = []
 
