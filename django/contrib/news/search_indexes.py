edcab3e70dfcb95689d2a307acea220c05cfe9a3
Brandon R. Stoner <github@monokro.me>
2010-08-22 21:08:31 +0000

    Added an article search index.

---
+++ django/contrib/news/search_indexes.py
@@ -0,0 +1,18 @@
+from haystack.indexes import *
+from haystack import site
+from models import Article
+
+class ArticleIndex(SearchIndex):
+    text = CharField(document=True, use_template=True)
+    title = CharField(model_attr='title')
+    body = CharField(model_attr='body')
+    summary = CharField(model_attr='summary')
+    slug = CharField(model_attr='slug')
+    author = CharField(model_attr='author')
+    categories = models.CharField(model_attr='category')
+
+    def get_queryset(self):
+        return Article.objects.all()
+
+site.register(Article, ArticleIndex)
+
