dbc5b18f4948a6b13455ba0efcd5501633acd8fa
Brandon R. Stoner <monokrome@monokro.me>
2010-09-13 19:26:00 -0600

    I fixed some stuffs.

--- setup.py
+++ setup.py
@@ -1,22 +1,13 @@
-from distutils.core import setup
+from distutils.core import setup, find_packages
 
 setup(name='django-news',
       version='0.7',
-      description='A django application for keeping your fans up to date...',
+      description='A django application for keeping your fans up to date.',
+      zip_safe=False,
+      include_package_data=True,
+      packages=find_packages(),
       author='Brandon R. Stoner',
       author_email='monokrome@monokro.me',
-      url='http://monokro.me/',
-      download_url='http://github.com/monokrome/django-news/',
-      packages=['django.contrib.news'],
-#      data_files=[
-#             (
-#                 'django/contrib/news/templates/news/',
-#                 [
-#                     'django/contrib/news/templates/news/base.html',
-#                     'django/contrib/news/templates/news/article_list.html',
-#                     'django/contrib/news/templates/news/article_detail.html',
-#                 ]
-#             ),
-#      ],
+      url='http://github.com/monokrome/django-news/',
 )
 
