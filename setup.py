4170bf6bbf24e548b05b455ec452f010949369f5
Brandon R. Stoner <monokrome@monokro.me>
2010-12-22 18:07:47 -0700

    Modified setup.py to use setuptools.

--- setup.py
+++ setup.py
@@ -1,14 +1,23 @@
-from distutils.core import setup
+try:
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
+except ImportError:
+    from ez_setup import use_setuptools
+    use_setuptools()
+    from setuptools import setup, find_packages
+    from setuptools.command.test import test
 
-setup(name='django-news',
-      version='0.7',
-      description='A django application for keeping your fans up to date.',
-      zip_safe=False,
-      include_package_data=True,
-      packages=['news'],
-      author='Brandon R. Stoner',
-      author_email='monokrome@monokro.me',
-      url='http://github.com/monokrome/django-news/',
+setup(name = 'django-news',
+      description = 'A django application for keeping your fans up to date.',
+      author = 'Brandon R. Stoner',
+      author_email = 'monokrome@monokro.me',
+      version = '0.7',
+
+      zip_safe = False,
+      include_package_data = True,
+      packages = find_packages(),
+      url = 'http://github.com/monokrome/django-news/',
+
       classifiers = [
           "Development Status :: 4 - Beta",
           "Environment :: Web Environment",
